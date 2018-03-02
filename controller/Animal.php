4a4d83d92f65d750419dbebcee1cfac4dd75deb9
DanielTorres <danieltorres31@outlook.com>
2018-03-02 01:20:15 +0000

    Criando DB

--- controller/Animal.php
+++ controller/Animal.php
@@ -5,15 +5,6 @@ class Animal {
     public function cadastrarAnimal() {
        require_once "Conexao.php";
        
-        $sql = "INSERT INTO `USUARIO`(`DES_EMAIL`, `DES_SENHA`, `DES_TIPO_USUARIO`, `NOM_USUARIO`) 
-        VALUES ('$p_EmailUsuario','$senha','$p_TipoUsuario','$p_NomeUsuario')";
-        if ($conn->query($sql) === true) {
-            return array("mensagem" => SUCESSO_USUARIO_CRIADO,
-                        "sucesso" => true);
-        } else {
-             return array("mensagem" => ERRO_USUARIO_CRIADO."Erro:".$conn->error,
-                          "sucesso" => false);
-        }
        
        $conn->close();
     }
@@ -24,7 +15,7 @@ class Animal {
         $sql = "
                 UPDATE ANIMAL
                 SET EXCLUIDO = 'T'
-                WHERE COD_ANIMAL = $id
+                WHERE COD_ANIMAL = '$id'
         ";
         
         if ($conn->query($sql) === true) {
@@ -44,7 +35,7 @@ class Animal {
         $sql ="
                 UPDATE ANIMAL
                 SET ADOTADO = 'T'
-                WHERE COD_ANIMAL = $id
+                WHERE COD_ANIMAL = '$id'
         ";
         
         if ($conn->query($sql) === true) {
