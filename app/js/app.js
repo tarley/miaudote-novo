3fb1eff7e9fe891c9a978f690fbe385aeec2083d
Henrique Batista <henriquecebrac37@gmail.com>
2018-03-08 22:36:41 +0000

    NG-ROUT aplicado na index

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,14 @@
-// var app = angular.module("miaudote", ['ngRoute','controller'])
-//           .config(function($routeProvider) {
-//             $routeProvider
-//               .when('/', {
-//                   templateUrl: '/',
-//                   controller: ''
-//               )};
-// });
+var app = angular.module("miaudote_js", ['ngRoute', 'miaudote.controller']);
+        app.config(function($routeProvider) {
+            $routeProvider
+                .when('/', {
+                    templateUrl: 'app/pages/inicial/_home.html',
+                    controller: 'MainController'
+                })
+                .when('/admin', {
+                  templateUrl: 'app/pages/gerencial/admin.html'
+                })
+                .otherwise({
+                 redirectTo: '/'
+                });
+});
