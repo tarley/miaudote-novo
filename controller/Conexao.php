c52969cf11db6f9223b1c51902e73802a960ec02
Nixon Sette <nixonsette@gmail.com>
2018-03-09 01:35:41 +0000

    Adicionando classe Instituição e modificações PDO de outras classes.

--- controller/Conexao.php
+++ controller/Conexao.php
@@ -8,6 +8,7 @@ try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->exec("set names utf8");
     }
 catch(PDOException $e)
     {
