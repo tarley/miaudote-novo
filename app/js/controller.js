3d8b7f2c0e8d29d315d5e54506452182bbefcc66
Nixon Sette <nixonsette@gmail.com>
2018-03-15 22:13:25 +0000

    Correção login

--- app/js/controller.js
+++ app/js/controller.js
@@ -456,7 +456,7 @@ var app = angular.module('miaudote.controller', [])
               data: "email="+email+"&senha="+senha,
               success: function (e){
                   if(e.sucesso){
-                       $("#").html("<div class=\"col-md-12\" style=\"border:1px solid #b3e096; background-color:#a2db7f; border-radius:4px;\">"+e.mensagem+"</div>");
+                       $("#mensagem").html("<div class=\"col-md-12\" style=\"border:1px solid #b3e096; background-color:#a2db7f; border-radius:4px;\">"+e.mensagem+"</div>");
                        window.location = "/#!/admin";
                   }else{
                       $("#mensagem").html("<div class=\"col-md-12\" style=\"border:1px solid #efa39b; background-color:#f7ded7; border-radius:4px;\">"+e.mensagem+"</div>");
