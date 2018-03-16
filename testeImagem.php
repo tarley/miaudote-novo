250eabd8f07665211578cf04c713eec02620260e
DanielTorres <danieltorres31@outlook.com>
2018-03-16 01:15:11 +0000

    Testando upload de imagem

--- testeImagem.php
+++ testeImagem.php
@@ -30,8 +30,8 @@ if ( isset( $_FILES[ 'arquivo' ][ 'name' ] ) && $_FILES[ 'arquivo' ][ 'error' ] 
         $novoNome = uniqid ( time () ) . '.' . $extensao;
  
         // Concatena a pasta com o nome
-        $destino = '/testeImagem' . $novoNome;
- 
+        $destino = 'testeImagem/' . $novoNome;
+        echo  $destino;
         // tenta mover o arquivo para o destino
         if ( @move_uploaded_file ( $arquivo_tmp, $destino ) ) {
             echo 'Arquivo salvo com sucesso em : <strong>' . $destino . '</strong><br />';
